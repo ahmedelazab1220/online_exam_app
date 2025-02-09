d34523bf12f95978d1721cb0343d50781dd37f60
mohamedamr77 <moamr947@gmail.com>
2025-02-09 02:01:11 +0200

    refactor(sign up): use appbar from scaffold and remove custom app bar in column

--- lib/features/auth/sign_up/presentation/view/sign_up_view.dart
+++ lib/features/auth/sign_up/presentation/view/sign_up_view.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:online_exam_app/core/utils/extenstion/translations.dart';
 import 'package:online_exam_app/features/auth/sign_up/presentation/view/widgets/sign_up_body.dart';
+
+import '../../../../../core/assets/app_colors.dart';
+import '../../../../../core/navigation/navigation_manager.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SignUpBody()
+    return Scaffold(appBar: _buildAppBar(context), body: SignUpBody());
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      titleSpacing: 0,
+      leading: IconButton(
+          onPressed: onPressedButtonLeading,
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: AppColors.black,
+          )),
+       title: Text(context.signUp, style: Theme.of(context).textTheme.titleLarge,),
     );
   }
+
+  void onPressedButtonLeading() {
+    NavigationManager.goBack();
+  }
+
 }
--- lib/features/auth/sign_up/presentation/view/widgets/sign_up_body.dart
+++ lib/features/auth/sign_up/presentation/view/widgets/sign_up_body.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:online_exam_app/core/assets/app_colors.dart';
-import 'package:online_exam_app/core/navigation/navigation_manager.dart';
-import 'package:online_exam_app/core/utils/extenstion/translations.dart';
-
-import 'custom_app_bar.dart';
 
 class SignUpBody extends StatelessWidget {
   const SignUpBody({super.key});
@@ -14,9 +9,7 @@ class SignUpBody extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Column(
-          children: [
-            CustomAppBar(title: context.signUp,),
-          ],
+          children: [],
         ),
       ),
     );
