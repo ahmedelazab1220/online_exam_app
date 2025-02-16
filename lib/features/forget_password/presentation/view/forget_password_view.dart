22c6b9532ee991e20dfdc647abfcdda52b94076d
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2025-02-17 00:48:32 +0200

    feat(forget_password): Integrate ForgetPasswordCubit into ForgetPasswordView
    - Wrapped ForgetPasswordView with BlocProvider to provide ForgetPasswordCubit.
    - Added BlocListener to handle navigation when NavigateToLoginScreenState is emitted.
    - Injected ForgetPasswordCubit using getIt for dependency management.
    - Updated back button action to trigger NavigateToLoginScreenAction() instead of manually popping the screen.
    
    Modified:
    - lib/features/forget_password/presentation/view/forget_password_view.dart

--- lib/features/forget_password/presentation/view/forget_password_view.dart
+++ lib/features/forget_password/presentation/view/forget_password_view.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:online_exam_app/core/utils/extenstion/translations.dart';
+import '../../../../core/di/di.dart';
+import '../../../../core/navigation/routes.dart';
+import '../view_model/forget_password_cubit/forget_password_cubit.dart';
 import 'widgets/forget_password_form.dart';
 
-class ForgetPasswordView extends StatelessWidget {
+class ForgetPasswordView extends StatefulWidget {
   const ForgetPasswordView({super.key});
 
   @override
+  State<ForgetPasswordView> createState() => _ForgetPasswordViewState();
+}
+
+class _ForgetPasswordViewState extends State<ForgetPasswordView> {
+  ForgetPasswordCubit viewModel = getIt.get<ForgetPasswordCubit>();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(context.password),
-        leading: InkWell(
-          onTap: () {
-            // just for test when add viewModel i'll remove it.
-            Navigator.pop(context);
-          },
-          child: const Icon(Icons.arrow_back_ios_new),
+    return BlocProvider<ForgetPasswordCubit>(
+      create: (context) => viewModel,
+      child: BlocListener<ForgetPasswordCubit, ForgetPasswordState>(
+        listener: (context, state) {
+          if (state is NavigateToLoginScreenState) {
+            Navigator.pop(context, AppRoutes.loginRoute);
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(context.password),
+            leading: InkWell(
+              onTap: () {
+                viewModel.doIntent(NavigateToLoginScreenAction());
+              },
+              child: const Icon(Icons.arrow_back_ios_new),
+            ),
+          ),
+          body: ForgetPasswordForm(),
         ),
       ),
-      body: ForgetPasswordForm(),
     );
   }
 }
