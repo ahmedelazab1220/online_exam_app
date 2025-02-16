7874252978ddb4acbc205d0de79eccbacb5dc639
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2025-02-16 21:31:31 +0200

    feat(auth): add forget password domain layer - Added ForgetPasswordUseCase for handling forgot password logic. - Created AuthRepository interface with orgetPassword method. - Introduced ForgetPasswordEntity to represent API response. - Implemented Result<T> sealed class for handling API results.
    New files:
    - lib/domain/core/api_result.dart
    - lib/domain/entities/forget_password_entity.dart
    - lib/domain/repositories/auth_repository.dart
    - lib/domain/use_cases/forget_password_use_case.dart

---
+++ lib/domain/use_cases/forget_password_use_case.dart
@@ -0,0 +1,13 @@
+import '../core/api_result.dart';
+import '../entities/forget_password_entity.dart';
+import '../repositories/auth_repository.dart';
+
+class ForgetPasswordUseCase {
+  final AuthRepository repository;
+
+  ForgetPasswordUseCase({required this.repository});
+
+  Future<Result<ForgetPasswordEntity>> call({required String email}) async {
+    return await repository.forgetPassword(email: email);
+  }
+}
